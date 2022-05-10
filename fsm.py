abbafbab0508be04c62ba1b94e9c4c7d360ac992
frake23 <dimsacom@gmail.com>
2022-05-10 22:30:04 +0300

    fsm completed

--- fsm.py
+++ fsm.py
@@ -29,13 +29,14 @@ def next_state(msg: str):
             event = list(filter(fun, events))[0]
             match event['name'][-1]:
                 case '3' | '4' as good_num:
-                    return {'state': event['to'], 'msg': f'\nВыдан товар {"A" if good_num == "3" else "B"}\nБез сдачи\n'}
+                    return {'state': event['to'], 'msg': f'\nВыдан товар {"A" if good_num == "3" else "B"}\n'}
                 case '0':
                     return {'state': event['to'], 'msg': f'\nПолучено {num} рублей\n'}
-        case 'cancel':
+        case 'cancel' | '' as action:
             fun = lambda e: e['from'] == current_state and e['name'][0] == 'e'
             event = list(filter(fun, events))[0]
-            return {'state': event['to'], 'msg': f'\nПокупка отменена\n'}
+            msg = '\nПокупка отменена\n' if action == 'cancel' else ''
+            return {'state': event['to'], 'msg': f'{msg}Сдача: {event["name"][-1]} рублей\n'}
 
 
 while True:
@@ -43,12 +44,10 @@ while True:
     match current_state:
         case 's0':
             print('Выберите товар A [1 рубль] или B [2 рубля] (type "A" or "B")')
-        case 's1' | 's2':
+        case 's1' | 's2' | 's3':
             print('Выберите действие: вставить 1 рубль, 2 рубля, отменить (type "1", "2" or "cancel")')
-        case 's3':
-            print('')
         case 's4':
-            print('')
+            print('Получите сдачу')
             read_msg = False
     next_st = next_state(input() if read_msg else '')
     current_state = next_st['state']
