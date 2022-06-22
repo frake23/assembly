1f27dc9ba4a8beb41413232acb8abcb3d2fe7f4e
Dmitry Suleymanov <frakex@yandex-team.ru>
2022-06-22 05:06:58 +0300

    dz2 completed

--- dz2/analyzer.py
+++ dz2/analyzer.py
@@ -2,36 +2,81 @@ delims = ' ();='
 func_words = ['if', 'else']
 
 
+def empty(f):
+    def _w(s: str):
+        if s == '':
+            return s, False
+        return f(s)
+    return _w
+
+
 def first_word(s: str):
     def find_first_delim_index(s: str):
-        res = s.find(next(x for x in s if x in delims))
-        return res if res != -1 else len(s) - 1
+        try:
+            res = s.find(next(x for x in s if x in delims))
+            return res - 1
+        except:
+            return len(s) - 1
 
     return s[:find_first_delim_index(s) + 1]
 
 
+@empty
 def id(s: str):
     s = s.lstrip()
     word = first_word(s)
     valid = word.isalpha() and not (word in func_words)
     if valid:
-        print(f'Identifier: #{word}')
+        print(f'Идентификатор: {word}')
         s = s[len(word):]
-    return s, valid
+        return s, valid
+    else:
+        raise Exception(f'Неверный идентификатор: {word}')
 
 
+@empty
+def number(s: str):
+    s = s.lstrip()
+
+    s1 = s
+    if s[0] == '-':
+        s1 = s[1:]
+
+    word = first_word(s1)
+
+    if word.isnumeric():
+        print(f'Число: {word}')
+        return s1[len(word):], True
+
+    return s, False
+
+
+@empty
 def assignment(s: str):
-    pass
+    s = s.lstrip()
+    if s == '' or s[0] != '=':
+        raise Exception(f'Ожидался терминал: =')
+
+    s1 = s[1:].lstrip()
+    s1, valid = number(s1)
+    if valid:
+        return s1, valid
+
+    s1, valid = id(s1)
+
+    return s1, True
 
 
+@empty
 def else_condition(s: str):
     s = s.lstrip()
     word = first_word(s)
     if word != 'else':
         return s, True
+    print('Служебное слово: else')
 
     s1 = s[len(word):].lstrip()
-    s1, valid = expression(s)
+    s1, valid = expression(s1)
 
     if not valid:
         return s, False
@@ -39,41 +84,38 @@ def else_condition(s: str):
     return s1, True
 
 
+@empty
 def condition(s: str):
     s = s.lstrip()
     word = first_word(s)
     if word != 'if':
         return s, False
+    print('Служебное слово: if')
 
     s1 = s[len(word):].lstrip()
-    if s1[0] != '(':
-        return s, False
+    if s1 == '' or s1[0] != '(':
+        raise Exception('Ожидался терминал: (')
 
     s1 = s1[1:].lstrip()
     s1, valid = id(s1)
-    if not valid:
-        return s, False
 
     s1 = s1.lstrip()
     if s1[0] != ')':
-        return s, False
+        raise Exception('Ожидался терминал: )')
 
     return s1[1:], True
 
 
+@empty
 def expression(s: str):
     s, valid = condition(s)
 
     if valid:
         s, valid = expression(s)
-        if not valid:
-            return s, valid
         s, valid = else_condition(s)
         return s, valid
 
     s, valid = id(s)
-    if not valid:
-        return s, valid
     s, valid = assignment(s)
     return s, valid
 
@@ -82,14 +124,17 @@ def simple_expression(s: str):
     s, valid = expression(s)
     s = s.strip()
 
-    if s[0] != ';':
-        return False
+    if s == '' or s[0] != ';':
+        raise Exception('Ожидался терминал: ;')
 
     return valid
 
 
-while s := input() != 'exit':
-    if simple_expression(s):
+print("Вводите строки до exit")
+while (s := input()) != 'exit':
+    try:
+        simple_expression(s)
         print('Строка валидна')
-    else:
+    except Exception as e:
+        print(str(e))
         print('Строка не валидна')
